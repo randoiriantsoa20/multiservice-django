f9ceecbbaeb0772180a1b1bd119ce761c39251a5
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-07-27 22:05:59 +0300

    Update build.sh
    Commande de démarrage sur Render

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'M@nager1!')
+"
