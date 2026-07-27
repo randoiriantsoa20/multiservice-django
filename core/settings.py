f25ac2f82a8699ad71cf8730c19b3d567ba810d2
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-07-27 22:41:51 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',  # Notre application
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
