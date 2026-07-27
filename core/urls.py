552b42c9ef7ef72ee3e25a63bb2218801b792a7e
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-07-27 22:12:24 +0300

    Update urls.py
    redirection /admin

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/', permanent=False)),
     path('admin/', admin.site.urls),
 ]
