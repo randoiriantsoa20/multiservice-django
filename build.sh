ae00fa7b79d97c457d81173e3a58e3a5d1fdfc74
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-07-27 23:03:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ python manage.py collectstatic --no-input
 
 python manage.py shell -c "
 from core.models import Utilisateur
-if not Utilisateur.objects.filter(identifiant='admin').exists():
-    user = Utilisateur(identifiant='admin', nom_complet='Administrateur', role_privilege='admin')
-    user.set_password('M@nager1')
-    user.save()
+user, created = Utilisateur.objects.get_or_create(
+    identifiant='admin',
+    defaults={'nom_complet': 'Administrateur', 'role_privilege': 'admin'}
+)
+user.set_password('M@nager1')
+user.save()
 "
