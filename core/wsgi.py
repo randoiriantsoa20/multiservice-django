fc8cecc79cefa5ba4effb6b5ba9783f1a4c5db94
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-08-02 06:19:46 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,6 +1,10 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# S'assurer que le registre des applications est bien chargé
+django.setup()
 
 application = get_wsgi_application()
