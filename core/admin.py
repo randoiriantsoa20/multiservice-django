27545437631765475392c8e34d6eedf958f3aff5
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-08-02 08:22:07 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,12 @@ from django.contrib.auth import get_user_model, login
 from django.contrib.auth.hashers import check_password as django_check_password
 from django import forms
 
+# Importation des modèles métiers depuis models.py
+from .models import Service, Transaction, CaisseJournaliere
+
 Utilisateur = get_user_model()
 
+# --- Configuration du Formulaire & du Site Admin ---
 class CustomAdminLoginForm(forms.Form):
     username = forms.CharField(label="Identifiant")
     password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
@@ -47,8 +51,33 @@ class CustomAdminSite(admin.AdminSite):
 admin.site = CustomAdminSite()
 admin.sites.site = admin.site
 
+
+# --- Enregistrement des Modèles dans l'Admin ---
+
 @admin.register(Utilisateur)
 class UtilisateurAdmin(admin.ModelAdmin):
     list_display = ('id_utilisateur', 'identifiant', 'nom_complet', 'role_privilege')
     search_fields = ('identifiant', 'nom_complet')
     list_filter = ('role_privilege',)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id_service', 'nom_service', 'tarif_base', 'est_actif')
+    search_fields = ('nom_service',)
+    list_filter = ('est_actif',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id_transaction', 'date_heure', 'service', 'utilisateur', 'montant', 'mode_paiement')
+    search_fields = ('id_transaction', 'remarques')
+    list_filter = ('mode_paiement', 'date_heure', 'service')
+    date_hierarchy = 'date_heure'  # Navigation rapide par calendrier/date
+
+
+@admin.register(CaisseJournaliere)
+class CaisseJournaliereAdmin(admin.ModelAdmin):
+    list_display = ('date_jour', 'fond_de_caisse', 'total_entrees', 'total_sorties', 'solde_final', 'est_cloturee')
+    list_filter = ('est_cloturee', 'date_jour')
+    date_hierarchy = 'date_jour'
