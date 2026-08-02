a71c5a7e4238ddda370f0d71cab04d7b9393159d
Rando Iriantsoa <randoiriantsoa@gmail.com>
2026-08-02 06:02:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
-python manage.py shell -c "
-from core.models import Utilisateur
-user, created = Utilisateur.objects.get_or_create(
-    identifiant='admin',
-    defaults={'nom_complet': 'Administrateur', 'role_privilege': 'admin'}
-)
-user.set_password('M@nager1')
-user.role_privilege = 'admin'
-user.save()
-"
+python create_admin.py
